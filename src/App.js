3c56cf804d62035bb93be9a532e9c5f07a4bd68d
way <weizhengming@ahqianmo.com>
2019-10-12 17:42:28 +0800

    123

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import React,{useState,useContext,createContext} from 'react';
+import React, {useState, useMemo, PureComponent, useEffect, useCallback, useRef} from 'react';
 import './App.css';
 
-const CountContext = createContext(0);
-
-function Bar(){
-    const count = useContext(CountContext);
-    return (
-        <div>{count}</div>
-    )
+class Counter extends PureComponent {
+    render() {
+        const {props} = this;
+        return (
+            <h1 >{props.count}</h1>
+        )
+    }
 }
-function App(props) {
 
-    const [count, setCount] = useState(0);
+function useCounter(defaultCounter){
+    const [count, setCount] = useState(defaultCounter);
+    const it = useRef();
+
+    useEffect(()=>{
+        it.current = setInterval(()=>{
+            setCount((count)=>count+1)
+        },1000)
+    },[]);
+
+    useEffect(()=>{
+        if(count>=10){
+            clearInterval(it.current);
+        }
+    });
+
+    return [count,setCount]
+}
+
+function App(props) {
+    const [count, setCount] = useCounter(0);
     return (
         <div>
-            <CountContext.Provider value={count}>
             <p>You clicked {count} times</p>
+            <Counter count={count} />
             <button onClick={() => setCount(count + 1)}>
                 Click me
             </button>
-                <Bar/>
-            </CountContext.Provider>
         </div>
     );
 }
 
-export  default  App;
+export default App;
